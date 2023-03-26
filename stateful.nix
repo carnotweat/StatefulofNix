6a761a245b667d686d0c7513d671a82825078e07
carnotweat <jasitis@gmail.com>
2023-03-26 11:08:16 +0530

    minor

--- stateful.nix
+++ stateful.nix
@@ -1,9 +1,21 @@
-systemd.services.my_app = {
-      enable = true;
-      script = ''
-          mkdir -p /var/app/media/cache;
-          mkdir -p /var/app/media/image;
-          ${pkgs.php} ${my_app}/bin/console cache:clear --env=prod
-        '';
-      restartIfChanged = true;
-    };
+# systemd.services.my_app = {
+#       enable = true;
+#       script = ''
+#           mkdir -p /var/app/media/cache;
+#           mkdir -p /var/app/media/image;
+#           ${pkgs.php} ${my_app}/bin/console cache:clear --env=prod
+#         '';
+#       restartIfChanged = true;
+#     };
+systemd.user.services.ipfs-daemon = {
+   description = "IPFS Daemon";
+   serviceConfig = {
+     Type = "forking";
+     ExecStart = "${pkgs.ipfs}/bin/ipfs daemon";
+     ExecStop = "pkill ipfs";
+     Restart = "on-failure";
+   };
+   wantedBy = [ "default.target" ];
+ };
+
+ systemd.services.ipfs-daemon.enable = true;
